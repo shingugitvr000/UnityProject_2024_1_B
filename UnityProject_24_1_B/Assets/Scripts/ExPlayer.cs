7776a5a23b4a77e1a4d7a03a0a7689d0b51e9cc0
shingugitvr000 <shingugitvr000@gmail.com>
2024-04-03 14:53:35 +0900

    [Feat] 게임 리소스 추가 , Trigger 사용

--- UnityProject_24_1_B/Assets/Scripts/ExPlayer.cs
+++ UnityProject_24_1_B/Assets/Scripts/ExPlayer.cs
@@ -43,7 +43,17 @@ public class ExPlayer : MonoBehaviour
         {
             point = 0;                                      //충돌이 일어났을때 포인트를 0으로 해준다.
             gameObject.transform.position = new Vector3(0.0f, 3.0f, 0.0f);  //충돌했을때 위치를 초기화 
-            Debug.Log(collision.gameObject.name);           //해당 오브젝트의 이름을 출력한다. 
+            Debug.Log(collision.gameObject.tag);           //해당 오브젝트의 이름을 출력한다. 
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)         //트리어게 들어왔을 때 
+    {
+        if (other.CompareTag("Item"))       //CompareTag 함수는 지어진 Tag(item) 이름을 검사한다.
+        {
+            Debug.Log("아이템과 충돌함");
+            point += 10;                    //10점 포인트를 올린다. point = point + 10의 줄임 표현
+            Destroy(other.gameObject);      //파괴한다.
         }
     }
 }
